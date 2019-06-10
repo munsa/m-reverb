9ac4794acde60ddb013f68db610f92eafed87bfc
marc.monserrat <mmonserrat90@gmail.com>
2019-06-11 00:42:59 +0100

    FunctionUtils

---
+++ Source/Utils/FunctionUtils.cpp
@@ -0,0 +1,23 @@
+#include "../Utils/FunctionUtils.h"
+
+String textFromValueFunction(double value)
+{
+	double level = value * 100.0;
+
+	// Round
+	int intLevel = (int)(level * 100.0);
+	double roundedLevel = ((double)intLevel) / 100.0;
+
+	return juce::String(roundedLevel) + "%";
+}
+
+double valueFromTextFunction(String text)
+{
+	double level = text.removeCharacters("%").getDoubleValue();
+
+	// Round
+	int intLevel = (int)(level * 100.0);
+	double roundedLevel = ((double)intLevel) / 100.0;
+
+	return roundedLevel / 100.0;
+};
---
+++ Source/Utils/FunctionUtils.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "../JuceLibraryCode/JuceHeader.h"
+#include "../Processors/PluginProcessor.h"
+
+class Utils : public AudioProcessorEditor,
+	public Slider::Listener
+{
+public:
+
+
+private:
+
+};
